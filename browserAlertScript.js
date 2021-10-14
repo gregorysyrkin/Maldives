cc1bd9a32989c3a39e52ed74e2f47a579e827096
gregorysyrkin@gmail.com <gregorysyrkin@gmail.com>
2021-10-14 19:45:34 +0700

    beep sound alert

--- browserAlertScript.js
+++ browserAlertScript.js
@@ -1,15 +1,23 @@
-callback = () => { if (targetNode.textContent === "В эфире") alert("Время побеждать!!!") }
+callback = () => {
+    if (targetNode.textContent === "В эфире") {
+        var mp3 = 'https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3';
+        for (i = 0; i < 10; i++) {
+            setTimeout(function () { (new Audio(mp3)).play() }, i * 1000)
+            alert("Не забудьте взять крем от загара.");
+        }
+    }
+}
 
 const observer = new MutationObserver(callback)
 
 const targetNode = document.querySelector(".player__artist.ng-binding");
 
 const observerOptions = {
-  childList: true,
-  attributes: true,
-  characterData: true,
-  // Omit (or set to false) to observe only changes to the parent node
-  subtree: true
+    childList: true,
+    attributes: true,
+    characterData: true,
+    // Omit (or set to false) to observe only changes to the parent node
+    subtree: true
 }
 
 observer.observe(targetNode, observerOptions);
